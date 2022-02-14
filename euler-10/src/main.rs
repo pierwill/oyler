a7d23e9e5fff12dbb68ed04123cc7b231f2c6af9
pierwill <pierwill@users.noreply.github.com>
2022-02-14 13:05:44 -0600

    Solve problem 10

---
+++ euler-10/src/main.rs
@@ -0,0 +1,11 @@
+//! I used a crate; so sue me.
+
+use primal::Primes;
+
+const MAX: usize = 2 * usize::pow(10, 6);
+
+fn main() {
+    let primes: Vec<usize> = Primes::all().take_while(|p| *p < MAX).into_iter().collect();
+
+    println!("{}", primes.iter().sum::<usize>());
+}
