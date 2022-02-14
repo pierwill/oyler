bb5fa63bed194dc5805c71b5c7732dbea09e290f
pierwill <pierwill@users.noreply.github.com>
2022-02-14 12:58:00 -0600

    153 attempt

---
+++ euler-153/src/main.rs
@@ -0,0 +1,37 @@
+//! https://projecteuler.net/problem=153
+
+use gaussiant::GaussianInt;
+use num_traits::Zero;
+
+fn main() {
+    let n = isize::pow(10, 5);
+    let mut big_sum = 0;
+
+    for _n in 1..=n {
+        let set = gaussiant::get_pos_g_ints(_n);
+        let mut divisors = vec![];
+        let mut sum = 0;
+
+        for z in set {
+            if z != GaussianInt::zero() && z.0.re != 0 {
+                if z.divides(GaussianInt::new(_n, 0)) {
+                    divisors.push(z);
+                    sum += z.0.re;
+                    sum += z.0.im;
+                    big_sum += z.0.re;
+                    big_sum += z.0.im;
+                }
+            }
+        }
+
+        // format divisors as string
+        // let mut output = String::new();
+        // for d in divisors {
+        //     output.push_str(&(d.to_string() + " "));
+        // }
+
+        println!("{_n}: {divisors} {sum}");
+    }
+
+    println!("big sum: {}", big_sum);
+}
