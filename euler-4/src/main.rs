4b59c3cac7f18f5a4b8102b2970cec6bf4659f05
pierwill <pierwill@users.noreply.github.com>
2022-02-14 11:02:56 -0600

    solve problem 4

--- euler-4/src/main.rs
+++ euler-4/src/main.rs
@@ -1,5 +1,26 @@
+//! A palindromic number reads the same both ways. The largest palindrome made
+//! from the product of two 2-digit numbers is 9009 = 91 × 99.
+//!
+//! Find the largest palindrome made from the product of two 3-digit numbers.
+
+const MAX: usize = 999;
+
 fn main() {
-    assert!(palindrome(11));
+    let mut palindromes = vec![];
+
+    for a in 1..=MAX {
+        for b in 1..=MAX {
+            let p = a * b;
+            if palindrome(p) {
+                palindromes.push(p);
+            }
+        }
+    }
+
+    println!(
+        "Largest palindrome of products of 3-digit numbers: {}",
+        palindromes.iter().max().unwrap()
+    );
 }
 
 // Returns true if x is a palindrome.
