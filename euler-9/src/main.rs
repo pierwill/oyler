a1c3ea7f024fb5cc84777ee923a2c4cfb6d8fe63
pierwill <pierwill@users.noreply.github.com>
2022-02-14 12:20:55 -0600

    Solve problem 9

---
+++ euler-9/src/main.rs
@@ -0,0 +1,32 @@
+const MAX: usize = 1000;
+
+#[derive(Debug)]
+struct Triple(usize, usize, usize);
+
+impl Triple {
+    fn new(a: usize, b: usize, c: usize) -> Self {
+        Triple(a, b, c)
+    }
+}
+
+fn main() {
+    for a in 1..=MAX {
+        for b in 1..=MAX {
+            for c in 1..=MAX {
+                let t = Triple::new(a, b, c);
+                let conditions = is_pythagorean_triple(&t) && (a < b && b < c) && a + b + c == 1000;
+                if conditions {
+                    println!("{t:?}");
+                }
+            }
+        }
+    }
+}
+
+fn is_pythagorean_triple(t: &Triple) -> bool {
+    let a = t.0;
+    let b = t.1;
+    let c = t.2;
+
+    usize::pow(a, 2) + usize::pow(b, 2) == usize::pow(c, 2)
+}
